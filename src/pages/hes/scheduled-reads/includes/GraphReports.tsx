0d96387135479ea13b362601386e113ea2f0bc81
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 <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>
2024-09-14 15:00:46 +0530

    adding responsive design

--- src/pages/hes/scheduled-reads/includes/GraphReports.tsx
+++ src/pages/hes/scheduled-reads/includes/GraphReports.tsx
@@ -1,5 +1,5 @@
 import { FC, useCallback } from 'react';
-import Graph from "@/components/customUI/Graph";
+import Graph from '@/components/customUI/Graph';
 import { ApexOptions } from 'apexcharts';
 import { ChartData } from '@/store/hes/types/records/reports';
 import EmptyScreen from '@/components/customUI/EmptyScreen';
@@ -8,8 +8,9 @@ interface GraphComponentProps {
   data: ChartData;
 }
 
-const GraphComponent: FC<GraphComponentProps> = ({ data: graphResponseData }) => {
-
+const GraphComponent: FC<GraphComponentProps> = ({
+  data: graphResponseData
+}) => {
   const renderCharts = useCallback(() => {
     return Object.keys(graphResponseData).map((commandName) => {
       const data = graphResponseData[commandName];
@@ -19,46 +20,57 @@ const GraphComponent: FC<GraphComponentProps> = ({ data: graphResponseData }) =>
           chart: {
             type: 'pie',
             width: '100%',
-            height: 350,
+            height: 350
           },
           labels: data.labels,
           legend: {
             position: 'bottom',
-            horizontalAlign: 'left',
+            horizontalAlign: 'left'
           },
           dataLabels: {
             enabled: true,
             formatter: (val: number) => `${val.toFixed(1)}%`,
             style: {
-              fontSize: '12px',
+              fontSize: '12px'
             },
-            minAngleToShowLabel: 0,
+            minAngleToShowLabel: 0
           },
 
-          responsive: [{
-            breakpoint: 658,
-            options: {
-              chart: {
-                width: 250,
-                height: 250
-              },
+          responsive: [
+            {
+              breakpoint: 658,
+              options: {
+                chart: {
+                  width: 250,
+                  height: 250
+                }
+              }
             }
-          }],
-          colors: ['#0A3690', '#FF5A5A', '#FFC32E', '#00C4B4', '#9966FF', '#FF9F40'],
+          ],
+          colors: [
+            '#0A3690',
+            '#FF5A5A',
+            '#FFC32E',
+            '#00C4B4',
+            '#9966FF',
+            '#FF9F40'
+          ]
         } as ApexOptions
       };
 
       return (
-        <div key={commandName} className="bg-white rounded-sm p-3 drop-shadow-sm max-w-full h-auto ">
+        <div
+          key={commandName}
+          className="bg-white rounded-sm p-3 drop-shadow-sm max-w-full h-auto "
+        >
           <Graph data={graphData} title={commandName} type="pie" />
         </div>
       );
     });
-  }, [ graphResponseData ]);
+  }, [graphResponseData]);
 
-  if(!graphResponseData || 
-    !Object.keys(graphResponseData).length 
-  ) return <EmptyScreen title={`scheduled reads not available`} />
+  if (!graphResponseData || !Object.keys(graphResponseData).length)
+    return <EmptyScreen title={`scheduled reads not available`} />;
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 pt-3">
