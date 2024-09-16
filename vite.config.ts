1a66f9201566d283b87aa10b22358a97ef64572d
azhar-polaris <azhar@polarisgrids.com>
2024-09-16 19:24:44 +0530

    UPDATED: removed build visualizer from build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import viteCompression from 'vite-plugin-compression';
-import { visualizer } from 'rollup-plugin-visualizer';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
     viteCompression(),
-    visualizer({ open: true })
+    // visualizer({ open: true })
   ],
   build: {
     target: 'esnext',
