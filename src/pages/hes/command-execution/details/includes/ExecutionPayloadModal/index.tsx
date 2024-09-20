eedbccf6e9531932ec212836b41c64835e8781c9
azhar-polaris <azhar@polarisgrids.com>
2024-09-20 18:10:47 +0530

    ADDED: command exec response screen

--- src/pages/hes/command-execution/details/includes/ExecutionPayloadModal/index.tsx
+++ src/pages/hes/command-execution/details/includes/ExecutionPayloadModal/index.tsx
@@ -28,26 +28,28 @@ const ExecutionPayloadModal: FC<ExecutionHistoryProps> = ({ data: { payload } })
             modalClass="max-w-[80vw] py-0 flex flex-col"
             ButtonLogo={Eye}
         >
-            <DialogTitle className='font-semibold text-xl mt-5'>
+            <DialogTitle className='font-semibold text-xl mt-5 text-[#0A3690]'>
                 Command Response Payload
             </DialogTitle>
 
-            <div className='w-full overflow-x-scroll'>
-                <table className='table-fixed pt-5'>
+            <div className='w-full overflow-x-scroll scrollable-content'>
+                <table className='pt-5 w-[100%]'>
                     <thead>
-                        <tr>
+                        <tr className='table-header drop-shadow-md'>
                             {headers.map(header => (
-                                <th className='border-2 border-black' key={header}>
-                                    <span className='uppercase px-3 py-2'>{header}</span>
+                                <th key={header} className='text-nowrap px-3 py-2 text-center'>
+                                    <span className='uppercase font-bold text-[#0A3690] text-xs'>
+                                        {header}
+                                    </span>
                                 </th>
                             ))}
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <tr className='bg-white'>
                             {tableData.map((tData, index) => (
-                                <td className='border-2 border-black' key={`${tData}_${index}`}>
-                                    <span className='uppercase px-3 py-2'>{tData}</span>
+                                <td key={`${tData}_${index}`} className='bg-[#F3F9F9] text-nowrap px-3 py-2 text-center'>
+                                    <span className='text-xs lg:text-sm text-nowrap text-[#464E5F]'>{tData}</span>
                                 </td>
                             ))}
                         </tr>
