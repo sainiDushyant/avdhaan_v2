ea6712b22a5380d17d030d7f9ffaaa1dcf610b67
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 <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>
2024-09-25 14:18:27 +0530

    adding bug fixes and code optimisation

--- src/pages/hes/scheduled-reads/index.tsx
+++ src/pages/hes/scheduled-reads/index.tsx
@@ -51,7 +51,7 @@ const ScheduledReads = () => {
       <div className="flex relative flex-col mt-8">
         <div className="flex justify-between items-center mb-2 ">
           <h1 className="capitalize secondary-title lg:main-title">
-            <span className="font-bold text-[#0A3690]">Block Load</span>
+            <span className="font-bold text-[#0A3690]">Schedule Reads</span>
           </h1>
           <ToggleView view={view} setView={setView} />
         </div>
