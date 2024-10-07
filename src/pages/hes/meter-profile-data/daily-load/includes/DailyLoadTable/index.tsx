8873d6d207567bd440962172cf536ee3bd95069a
Dushyant Saini <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>
2024-10-07 18:07:38 +0530

    Fixed some during build errors

--- src/pages/hes/meter-profile-data/daily-load/includes/DailyLoadTable/index.tsx
+++ src/pages/hes/meter-profile-data/daily-load/includes/DailyLoadTable/index.tsx
@@ -1,16 +1,17 @@
 import { useMemo, useState } from 'react';
 import { useGetDailyLoadPushDataQuery } from '@/store/hes/hesApi';
 import { useLocation } from 'react-router-dom';
-import { getCommandExecutionHistoryUrlSearchParams } from '@/pages/hes/command-execution/includes/utils';
+import { getCommandExecutionHistoryUrlSearchParams } from '@/pages/hes/command/includes/utils';
 import MetricProfileTable from '../../../includes/MetricProfileTable';
 import { MeterProfileQueryParams } from '@/store/hes/types/records/meter-profile-data-metrics';
 import { useSelector } from '@/store';
 
 const DailyLoadTable = () => {
-
   const { search } = useLocation();
-  const [query, setQuery] = useState<MeterProfileQueryParams>({ sub_category: 1 });
-  const mainFilterLoading = useSelector(state => state.hes.mainFilterLoading);
+  const [query, setQuery] = useState<MeterProfileQueryParams>({
+    sub_category: 1
+  });
+  const mainFilterLoading = useSelector((state) => state.hes.mainFilterLoading);
 
   const urlSearchParams = useMemo(() => {
     return getCommandExecutionHistoryUrlSearchParams({ query, search });
@@ -28,15 +29,19 @@ const DailyLoadTable = () => {
     { skip: mainFilterLoading }
   );
 
-  return <MetricProfileTable
-    response={response}
-    isLoading={isLoading}
-    isFetching={isFetching}
-    isError={isError} error={error}
-    query={query} setQuery={setQuery}
-    refresh={refresh} 
-    filterType={'datetime'} 
-  />
+  return (
+    <MetricProfileTable
+      response={response}
+      isLoading={isLoading}
+      isFetching={isFetching}
+      isError={isError}
+      error={error}
+      query={query}
+      setQuery={setQuery}
+      refresh={refresh}
+      filterType={'datetime'}
+    />
+  );
 };
 
 export default DailyLoadTable;
--- src/pages/hes/meter-profile-data/monthly-billing/includes/BillingTable/index.tsx
+++ src/pages/hes/meter-profile-data/monthly-billing/includes/BillingTable/index.tsx
@@ -1,16 +1,17 @@
 import { useMemo, useState } from 'react';
 import { useGetMonthlyBillingDataQuery } from '@/store/hes/hesApi';
 import { useLocation } from 'react-router-dom';
-import { getCommandExecutionHistoryUrlSearchParams } from '@/pages/hes/command-execution/includes/utils';
+import { getCommandExecutionHistoryUrlSearchParams } from '@/pages/hes/command/includes/utils';
 import MetricProfileTable from '../../../includes/MetricProfileTable';
 import { MeterProfileQueryParams } from '@/store/hes/types/records/meter-profile-data-metrics';
 import { useSelector } from '@/store';
 
 const BillingTable = () => {
-
   const { search } = useLocation();
-  const [query, setQuery] = useState<MeterProfileQueryParams>({ sub_category: 1 });
-  const mainFilterLoading = useSelector(state => state.hes.mainFilterLoading);
+  const [query, setQuery] = useState<MeterProfileQueryParams>({
+    sub_category: 1
+  });
+  const mainFilterLoading = useSelector((state) => state.hes.mainFilterLoading);
 
   const urlSearchParams = useMemo(() => {
     return getCommandExecutionHistoryUrlSearchParams({ query, search });
@@ -28,15 +29,19 @@ const BillingTable = () => {
     { skip: mainFilterLoading }
   );
 
-  return <MetricProfileTable
-    response={response}
-    isLoading={isLoading}
-    isFetching={isFetching}
-    isError={isError} error={error}
-    query={query} setQuery={setQuery}
-    refresh={refresh} 
-    filterType={"monthyear"}  
-  />
+  return (
+    <MetricProfileTable
+      response={response}
+      isLoading={isLoading}
+      isFetching={isFetching}
+      isError={isError}
+      error={error}
+      query={query}
+      setQuery={setQuery}
+      refresh={refresh}
+      filterType={'monthyear'}
+    />
+  );
 };
 
 export default BillingTable;
